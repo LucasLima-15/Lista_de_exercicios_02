4832be70999d98ec7ce0fbd0716fea7af8846075
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-23 17:09:29 -0300

    first commit

---
+++ Ex.5/Program.cs
@@ -0,0 +1,3 @@
+﻿/*5. Um gerente de loja quer saber quantos produtos estão em estoque. O aluno deve solicitar que o usuário insira a quantidade de produtos
+ * recebidos e vendidos ató que của digito . Depois disso, o programa deve calcular a quantidade total de produtos em estoque, utilizando
+ * as três estruturas de repetição */
---
+++ Ex.9/Program.cs
@@ -0,0 +1,4 @@
+﻿/*
+ 9. Um diente deseja saber o total de suas compras em um supermercado. O aluno deve solicitar que o usuário insira o valor de cada compra
+até que ale digite 0. A soma dos valores das compras deve ser implementada usando as três estruturas de repetição. 
+ */
