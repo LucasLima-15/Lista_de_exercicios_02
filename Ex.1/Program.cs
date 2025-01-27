08177db12f8074d735e31ceffb00b5c5ff839c1b
LucasLima-15 <pedrolucas.ara2003@gmail.com>
2025-01-27 14:06:19 -0300

    Update Program.cs fim

--- Ex.1/Program.cs
+++ Ex.1/Program.cs
@@ -10,27 +10,72 @@ class Program
 {
 	static void Main()
 	{
+		/*utilizando */
 		int contador = 0;
 		int dia = 1;
 		
 		while (dia != 0)
 		{  
-		  Console.WriteLine("Digite o dia trabalhado (de 1 a 31, 0 encerra o programa): ");
-		  dia = int.Parse(Console.ReadLine());
-		  if (dia > 31)
-		  {
-		    Console.WriteLine("Dia inválido");
+			  Console.WriteLine("Digite o dia trabalhado (de 1 a 31, 0 encerra o programa): ");
+			  dia = int.Parse(Console.ReadLine());
+			  if (dia > 31)
+			  {
+			    Console.WriteLine("Dia inválido");
+			  }
+			  else if (dia < 0)
+			  {
+			    Console.WriteLine("Dia inválido");
+			  }  
+			  else if (dia != 0)
+			  {
+			    contador++;
+			  }
 		  }
-		  else if (dia < 0)
-		  {
-		    Console.WriteLine("Dia inválido");
-		  }  
-		  else if (dia != 0)
-		  {
-		    contador++;
-		  }
-	  }
-	  Console.WriteLine($"Você trabalhou {contador} dias do mês");
+		  Console.WriteLine($"Você trabalhou {contador} dias do mês");
 	  
+	}
+	{
+		/*utilizando DO*/
+		int dia = 1;
+	        int contador = 0;
+	        
+	        do
+	        {
+	            Console.WriteLine("Digite o dia trabalhado (0 encerra o programa): ");
+	            dia = int.Parse(Console.ReadLine());
+	            if (dia > 31)
+			    {
+			        Console.WriteLine("Dia inválido");
+			    }
+			    else if (dia < 0)
+			    {
+			        Console.WriteLine("Dia inválido");
+			    }  
+			    else if (dia != 0)
+	                contador++;
+	        }
+	        while (dia != 0);
+	        Console.WriteLine($"Você trabalhou {contador} dias do mês.");
+	}
+	{
+		/*utilizando FOR*/
+	        int contador = 0; 
+	        int dia = 1;
+	        for (; dia != 0;)
+	        {   
+	            Console.WriteLine("Digite o dia trabalhado (0 encerra o programa)");
+	            dia = int.Parse(Console.ReadLine());
+	            if (dia > 31)
+			    {
+			        Console.WriteLine("Dia inválido");
+			    }
+			    else if (dia < 0)
+			    {
+			        Console.WriteLine("Dia inválido");
+			    }  
+			    else if (dia != 0)
+	                contador++;
+	        }
+	        Console.WriteLine($"Você trabalhou {contador} dias do mês");
 	}
 }
