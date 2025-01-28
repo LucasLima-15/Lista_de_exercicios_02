26e6fad9a209d323f56ba04c94006894e27468da
LucasLima-15 <pedrolucas.ara2003@gmail.com>
2025-01-28 11:47:58 -0300

    Update Program.cs fim10

--- Ex.10/Program.cs
+++ Ex.10/Program.cs
@@ -3,3 +3,58 @@
 passagens vendidas. até que ele digte um valor negativo. O total de passagens vendidas deve ser calculado e apresentado utilizando while,
 do while e for 
  */
+
+using System;
+
+public class passagem
+{
+    public static void Main(string[] args)
+    {
+        /*Utilizando WHILE*/
+        int passagem = 1;
+        int total = 0;
+        
+        while (passagem >= 0)
+        {   
+            Console.WriteLine("Digite a quantidade de passagens vendidas (número negativo encerrar o programa): ");
+                passagem = int.Parse(Console.ReadLine());
+            if (passagem >= 0)
+            {
+                total += passagem;
+            }
+        }    
+        Console.WriteLine($"Você vendeu total de {total} passagem(s). ");    
+    {
+        /*Utilizando DO*/
+        int passagem = 1;
+        int total = 0;
+        
+        do
+        {   
+            Console.WriteLine("Digite a quantidade de passagens vendidas (número negativo encerrar o programa): ");
+                passagem = int.Parse(Console.ReadLine());
+            if (passagem >= 0)
+            {
+                total += passagem;
+            }
+        }
+        while (passagem >= 0);
+        Console.WriteLine($"Você vendeu total de {total} passagem(s). ");
+      {
+        /*Utilizando FOR*/
+        int passagem = 1;
+        int total = 0;
+        
+        for (; passagem >= 0;)
+        {   
+            Console.WriteLine("Digite a quantidade de passagens vendidas (número negativo encerrar o programa): ");
+                passagem = int.Parse(Console.ReadLine());
+            if (passagem >= 0)
+            {
+                total += passagem;
+            }
+        }
+
+        Console.WriteLine($"Você vendeu total de {total} passagem(s). ");    
+    }
+}
