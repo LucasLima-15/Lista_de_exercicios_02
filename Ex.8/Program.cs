c2bd492d91cc3737028ba49db434f9fd24af352c
LucasLima-15 <pedrolucas.ara2003@gmail.com>
2025-01-28 11:31:25 -0300

    Update Program.cs fim8

--- Ex.8/Program.cs
+++ Ex.8/Program.cs
@@ -3,3 +3,63 @@ B. Um atendente de suporte técnico deseja contar quantas chamadas atendeu em um
 duração de cada chamada em minutos até que ele digite . O programa deve calcular o total de minutos atendidos, utilizando while, do while
 e for
  */
+
+using System;
+
+public class Telefonemas
+{
+    public static void Main(string[] args)
+    {
+        /*Utilizando WHILE*/
+        int tempo = 0;
+        int vezes = 0;
+        int total = 0;
+        
+        while (tempo >= 0)
+        {   
+            Console.WriteLine("Digite a duração do telefonema em minutos(números negativos para encerrar o programa): ");
+                tempo = int.Parse(Console.ReadLine());
+                if (tempo >= 0) 
+                {
+                    vezes++;
+                    total += tempo;
+                }    
+        }    
+        Console.WriteLine($"Você passou {total} minutos em ligação de {vezes} telefonema(s). ");    
+    {
+        /*Utilizando DO*/
+        int tempo = 0;
+        int vezes = 0;
+        int total = 0;
+        
+        do
+        {   
+            Console.WriteLine("Digite a duração do telefonema em minutos(números negativos para encerrar o programa): ");
+                tempo = int.Parse(Console.ReadLine());
+                if (tempo >= 0) 
+                {
+                    vezes++;
+                    total += tempo;
+                }    
+        }
+        while (tempo >= 0);
+        Console.WriteLine($"Você passou {total} minutos em ligação de {vezes} telefonema(s). ");    
+    {
+        /*Utilizando FOR*/
+        int tempo = 0;
+        int vezes = 0;
+        int total = 0;
+        
+        for (; tempo >= 0;)
+        {   
+            Console.WriteLine("Digite a duração do telefonema em minutos(números negativos para encerrar o programa): ");
+                tempo = int.Parse(Console.ReadLine());
+                if (tempo >= 0) 
+                {
+                    vezes++;
+                    total += tempo;
+                }    
+        }    
+        Console.WriteLine($"Você passou {total} minutos em ligação de {vezes} telefonema(s). ");    
+    }
+}
